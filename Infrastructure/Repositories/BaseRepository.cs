ab88be6fb761c56e2465964cf5c91a30727153c4
Dmitriy Fomenko <dfomenko@mmtr.ru>
2021-06-09 20:30:54 +0300

    Return baseReposotory

---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Base;
+using Domain.Filter;
+using Infrastructure.AppSettings;
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public abstract class BaseRepository<TModel, TFilter>
+        where TModel : BaseModel
+        where TFilter : BaseFilterDto
+    {
+        protected readonly Context Context;
+        private readonly AppSettingsConfiguration _appSettingsConfiguration;
+
+        protected BaseRepository(Context context, AppSettingsConfiguration appSettingsConfiguration)
+        {
+            Context = context;
+            _appSettingsConfiguration = appSettingsConfiguration;
+        }
+
+        public virtual async Task<TModel> GetById(Guid guid)
+        {
+            var data = GetDataSet().FirstOrDefaultAsync(u => u.Id == guid && u.IsDelete == false);
+            return await data;
+        }
+
+        public async Task<TModel> Create(TModel data, Guid? creatorId = null)
+        {
+            data.CreatorId = creatorId;
+            data.DateCreated = DateTime.UtcNow;
+            data.IsActive ??= true;
+            data.DateUpdated = DateTime.UtcNow;
+            await Context.Set<TModel>().AddAsync(data);
+            await Context.SaveChangesAsync();
+            return data;
+        }
+
+        public async Task<TModel> Edit(TModel data)
+        {
+            data.DateUpdated = DateTime.UtcNow;
+            Context.Update(data);
+            await Context.SaveChangesAsync();
+            return data;
+        }
+
+        public async Task<TModel> Delete(TModel data)
+        {
+            data.IsDelete = true;
+            data.DateDelete = DateTime.Now;
+            data.DateUpdated = DateTime.UtcNow;
+            Context.Update(data);
+            await Context.SaveChangesAsync();
+            return data;
+        }
+
+        protected virtual IQueryable<TModel> ApplyPaging(IQueryable<TModel> source, FilterPagingDto paging)
+        {
+            paging ??= new FilterPagingDto {PageSize = _appSettingsConfiguration.DefaultPageSize};
+            return source
+                .Skip(paging.PageNumber * paging.PageSize)
+                .Take(paging.PageSize);
+        }
+
+        protected virtual IQueryable<TModel> GetDataSet()
+            => Context.Set<TModel>().AsNoTracking();
+
+        /// <summary>
+        /// Get filtered entities
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public virtual IEnumerable<TModel> GetFiltered(TFilter filter)
+        {
+            var result = GetDataSet();
+
+            result = ApplyFilter(result, filter);
+
+            result = ApplySort(result, filter.Sort);
+            result = ApplyPaging(result, filter.Paging);
+
+            return result;
+        }
+
+        protected virtual IQueryable<TModel> ApplySort(IQueryable<TModel> source, FilterSortDto sort)
+        {
+            sort ??= new FilterSortDto
+            {
+                ColumnName = nameof(BaseModel.DateCreated),
+                IsDescending = true
+            };
+
+            if (sort.ColumnName == nameof(BaseModel.DateCreated))
+                return sort.IsDescending
+                    ? source.OrderByDescending(p => p.DateCreated)
+                    : source.OrderBy(p => p.DateCreated);
+
+            if (sort.ColumnName == nameof(BaseModel.IsActive))
+                return sort.IsDescending
+                    ? source.OrderByDescending(p => p.IsActive)
+                    : source.OrderBy(p => p.IsActive);
+
+            if (sort.ColumnName == nameof(BaseModel.DateCreated))
+                return sort.IsDescending
+                    ? source.OrderByDescending(p => p.DateCreated)
+                    : source.OrderBy(p => p.DateCreated);
+
+            if (sort.ColumnName == nameof(BaseModel.IsActive))
+                return sort.IsDescending
+                    ? source.OrderByDescending(p => p.IsActive)
+                    : source.OrderBy(p => p.IsActive);
+
+            return source;
+        }
+
+        protected virtual IQueryable<TModel> ApplyFilter(IQueryable<TModel> result, TFilter filter)
+        {
+            if (filter.IsActive.HasValue)
+                result = result.Where(p => p.IsActive == filter.IsActive);
+
+            result = result.Where(p => p.IsDelete == false);
+
+            return result;
+        }
+
+
+        public virtual async Task<int> GetCount(TFilter filter)
+        {
+            var result = Context.Set<TModel>().AsNoTracking();
+            result = ApplyFilter(result, filter);
+
+            return await result.CountAsync();
+        }
+    }
+}
